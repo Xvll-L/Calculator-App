2d92c3c96c25cb122049c63e2ea35351507ec285
Ahmed <ahmedmo501296@gmail.com>
2022-07-12 18:54:23 +0100

    out value of buttons

--- App.js
+++ App.js
@@ -4,12 +4,35 @@ const DisplayBottom = document.querySelector(".bottomValue-display")
 
 const buttons = document.querySelectorAll(".buttons")
 
-console.log(buttons)
+//disbale dot
+const dot = document.querySelector(".btn-dot").disable = true
+test2 = []
+for(let i = 0; i < buttons.length;i++){
+    //disbale dot
+    const dot = document.querySelector(".btn-dot").disable = true
+    buttons[i].addEventListener("click", 
+    function(e){ 
+        //disbale dot
+        const dot = document.querySelector(".btn-dot").disable = true
+        test =  e.target.value
+        
+        test2.push(test)
+       
+       
+    
+    })
+} 
+//DisplayBottom.textContent = 
+test2.array.forEach(element => {
+    
+    console.log(element)
+});
+ 
 
-buttons.addEventListener("click", 
-    function(){
+/*buttons.addEventListener("click", 
+function(){
 
-})
+})*/
 
 
 
