bf20df735107e1253dd0e84f72cb5f02c4659d17
Ahmed <ahmedmo501296@gmail.com>
2022-07-12 15:58:01 +0100

    add const and functions

---
+++ App.js
@@ -0,0 +1,22 @@
+// what will be displayed to the user
+const DisplayTop = document.querySelector(".topValue-display")
+const DisplayBottom = document.querySelector(".bottomValue-display")
+
+const buttons = document.querySelectorAll(".buttons")
+
+console.log(buttons)
+
+buttons.addEventListener("click", 
+    function(){
+
+})
+
+
+
+function calulator(values){
+
+}
+
+function add(value1,value2){
+    return value1 + value2
+}
