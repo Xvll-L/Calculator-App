0215f1c6f8d45d9fcd8928f3890ccab1311f7b37
Ahmed <ahmedmo501296@gmail.com>
2022-07-22 19:37:15 +0100

    fresh start

--- App.js
+++ App.js
@@ -4,6 +4,17 @@ const DisplayBottom = document.querySelector(".bottomValue-display")
 
 const buttons = document.querySelectorAll(".buttons")
 
+const operates = document.querySelectorAll(".operates-btns")
+
+
+
+//global variables
+const cal = {
+    firstValues: 0,
+    secondValue: 0,
+    results: null,
+    operator: null
+}
 
 
 // This will loop all the buttions but not the operate
@@ -12,138 +23,40 @@ for(let i = 0; i < buttons.length;i++){
     buttons[i].addEventListener("click", 
     function(e){ 
        let  values =  e.currentTarget.value
-        calulator(values)
        
+      conVal = DisplayBottom.textContent += values + ""
         
+       cal.firstValues = parseInt(conVal)
+       operate()
+
     })
+
 
 } 
 
-const operates = document.querySelectorAll(".operates-btns")
+ operates.forEach(btns => {
+        btns.addEventListener("click",function(e){
+            opes = e.currentTarget.value
+            clear(opes)
+           
+console.log(cal) 
+        })
+ });
 
-operates.forEach(btns => {
-   btns.addEventListener("click",function(e){
-    opes = e.currentTarget.value
-    clear(opes)
-    calulator(undefined, opes)
-   })
-});
 
-// get user inputs 
-// trim user input after they press add, divide subtract or multiply
-// store the trimmed data as value 2
-//???
-//Concatenating numbers
-let secondValue
-let numberValues = " ";
-function calulator(values){
 
-    //object to store first and second inputs
-    let cal = {
-        value1: 1,
-        value2: 1
-    }
-   
-    //disable button when it overflow
-  /* if (DisplayBottom.textContent.length === 10)
-    {
-        for(let i = 0; i < buttons.length;i++){
-            buttons[i].setAttribute('disabled', true)
-        }
-        DisplayTop.textContent = "Buttons disabled"
-    }*/
 
-    
-    let toint = parseInt(values)
-    if(Number.isInteger(toint)){
-    
-        cal.value1 = parseInt(DisplayBottom.textContent += toint + "")
-        
-        
-        
-    //cal.value1[numberValues] 
-       
-        
-    }
-    
-     console.log(cal)
-    
-<<<<<<< HEAD
-    
+function operate(){
+     DisplayTop.textContent = cal.firstValues
  
-    secondValue = DisplayTop.textContent
-=======
-    numberValues = numberValues + values + ""  
-    secondValue = (DisplayTop.textContent)
->>>>>>> parent of 1cb3e76 (Calculator add, subtract,  multiply  and divide)
-    test2 = parseInt(secondValue)
-    cal.value2[test2]
-  
-
-    
-
-    
-    
-    
-    //DisplayBottom.textContent = DisplayBottom.textContent + values + "";
-    if (values === "clear"){
-        DisplayTop.textContent = " "
-        DisplayBottom.textContent = ""
-        
-        cal.value1[0]
-        cal.value2[0]
-    }
-<<<<<<< HEAD
-    
-    
-
-   
-    
-
-   if(values === "+" ){
-    DisplayTop.textContent = `${toint} + `
-    DisplayBottom.textContent = "0"
->>>>>>> parent of 351cf81 (bug - operators and button event need separation)
-    numberValues = " "  
-       
-<<<<<<< HEAD
-        
-=======
-            console.log(add(cal.value1,cal.value2))
-            if(add(cal.value1,cal.value2) > 1){
-                DisplayTop.textContent = `${add(cal.value1,cal.value2)} + `
-            }
-    
-    } else if (values === "-" ) {
-        DisplayTop.textContent = `${toint} - `
-        DisplayBottom.textContent = "0"
-        numberValues = " "  
-    } else if (values === "*"){
-        DisplayTop.textContent = `${toint} * `
-        DisplayBottom.textContent = "0"
-        numberValues = " "  
-    } else if(values === "/"){
-        DisplayTop.textContent = `${toint} / `
-        DisplayBottom.textContent = "0"
-        numberValues = " "  
-    }
-       
->>>>>>> parent of 1cb3e76 (Calculator add, subtract,  multiply  and divide)
-   
-}
-function operate(value1, value2, operates){
-    
-    if (value1 =! null){
-        
-    }
     
 }
+
 function add(value1,value2){
     return value1+value2
 }
 
 
-<<<<<<< HEAD
 function multiply(value1,value2){
     return value2 * value1
 }
@@ -155,10 +68,10 @@ function divide(value1,value2){
 function clear(values){
     
     if(values === "clear" ){
-        DisplayTop.textContent = " "
-        DisplayBottom.textContent = ""
+        cal.firstValues = 0
+        cal.secondValue = 0,
+        cal.results = null,
+        cal.operator = null
     } 
     return 0
 }
-=======
->>>>>>> parent of 1cb3e76 (Calculator add, subtract,  multiply  and divide)
